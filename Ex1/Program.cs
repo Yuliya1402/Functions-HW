3621d43a8155052487bf783e4457e6b50cbb89b5
Yuliya Turmacheva <Yuliyat1402@gmail.com>
2024-01-25 01:19:30 +0300

    Home work

---
+++ Ex1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число или q для выхода");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "q")
+            {
+                Console.WriteLine("Программа завершена");
+                break;
+            }
+
+            if (int.TryParse(input, out int num))
+            {
+                int digitSum = CalculateDigitSum(num);
+
+                if (digitSum % 2 == 0)
+                {
+                    Console.WriteLine("Программа завершена");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Сумма цифр числа нечетная.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+        }
+    }
+
+    static int CalculateDigitSum(int num)
+    {
+        int sum = 0;
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
+
---
+++ Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      int arraySize = 10;
+      Random rand = new Random();
+      int[] array = new int [arraySize];
+      for(int i = 0; i < arraySize; i++) 
+      {
+        array[i] = rand.Next(100, 999);
+      }
+      
+      foreach(int elem in array)
+      {
+        Console.Write(elem +" ");
+      }
+      
+      int count = 0;
+      foreach(int elem in array)
+      {
+        if(elem % 2 == 0)
+        {
+          count++;
+        }  
+      }
+      Console.Write(count);
+     }
+}
