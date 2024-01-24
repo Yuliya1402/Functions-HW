16cb37b30fecb4094c2a57c6c3ec0971fcd94c90
Yuliya Turmacheva <Yuliyat1402@gmail.com>
2024-01-25 01:22:44 +0300

    3

---
+++ Ex3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      int arraySize = 10;
+      int[] arr = new int[arraySize];
+      for(int i = 0; i < arraySize; i++)
+      {
+        arr[i] = i+1;
+        Console.Write(arr[i]+" ");
+      }
+      ReverseArray(arr);
+      PrintArray(arr);
+     }
+    static void  ReverseArray(int[] arr)
+    {
+      int left = 0;
+      int right = arr.Length - 1;
+      while(left < right)
+      {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+      }
+    }
+    static void PrintArray(int[] arr)
+   {
+    foreach(int elem in arr)
+     {
+       Console.Write($"{elem} ");
+     }
+    }
+}
